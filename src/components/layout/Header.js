03793e20c70eeede90b6f51eacac6898dc58437f
Christie Hui <cwsh15@gmail.com>
2020-08-19 15:17:24 -0700

    added logo and custom button color

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-import HonLogo from '../../svg/HonLogo4';
+// import HonLogo from '../../svg/HonLogo4';
+import LogoPNG from '../../images/HonLogo3.png';
 
 const Header = () => (
   <header className="sticky top-0 bg-white shadow">
     <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
       <div className="flex items-center text-3xl">
         <AnchorLink className="ml-5" href="#top">
-          <HonLogo style={{ height: '40px', width: '55px' }} />
+          {/* <HonLogo style={{ height: '40px', width: '55px' }} /> */}
+          <img
+            className="mx-auto"
+            src={LogoPNG}
+            alt="logo"
+            style={{ height: '40px', width: '60px' }}
+          />
         </AnchorLink>
       </div>
       <div className="flex mt-4 sm:mt-0">
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
           lighter: 'hsl(240, 44%, 40%)',
           default: 'hsl(240, 44%, 49%)',
           darker: 'hsl(240, 44%, 54%)'
-        }
+        },
+        button: '#0000ff'
       }
     },
     container: {
