15cd41e08b2342731e1270f5804852b2963c8422
Christie Hui <cwsh15@gmail.com>
2020-08-18 21:41:18 -0700

    updated favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,14 @@ module.exports = {
       options: {
         tailwindConfig: `tailwind.config.js`
       }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'hon-media-sd',
+        start_url: '/',
+        icon: 'src/images/HonLogo.png'
+      }
     }
   ]
 };
