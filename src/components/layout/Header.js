d2d36b6c20721a5857b119da0fcb362994d35573
Christie Hui <cwsh15@gmail.com>
2020-08-19 14:45:24 -0700

    update logo

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-import LogoPNG from '../../images/HonLogo2.png';
+import HonLogo from '../../svg/HonLogo4';
 
 const Header = () => (
   <header className="sticky top-0 bg-white shadow">
     <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
-      <div className="flex items-center text-2xl">
-        <AnchorLink className="w-12 ml-5" href="#top">
-          <img className="mx-auto w-full" src={LogoPNG} alt="logo" />
+      <div className="flex items-center text-3xl">
+        <AnchorLink className="ml-5" href="#top">
+          <HonLogo style={{ height: '40px', width: '55px' }} />
         </AnchorLink>
       </div>
       <div className="flex mt-4 sm:mt-0">
--- src/pages/index.js
+++ src/pages/index.js
@@ -68,8 +68,8 @@ export default () => (
     />
     <section id="about" className="py-20 lg:pt-32">
       <div className="container mx-auto text-center">
-        <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">Our Team</h1>
-        <div className="mt-12 flex flex-col space-x-8 sm:flex-row mt-10 lg:px-33">
+        <h2 className="text-3xl lg:text-5xl font-semibold">Our Team</h2>
+        <div className="mt-16 flex flex-col space-x-8 sm:flex-row mt-10 lg:px-33">
           <div className="w-full h-full md:w-1/4">
             <StatsBox image={Steven} secondaryText="Steven Phung" />
             <p className="text-gray-600 mx-auto px-4 text-lg">Co-Founder/CEO</p>
@@ -104,7 +104,7 @@ export default () => (
         </div>
       </div>
     </section>
-    <section id="services" className="py-20 -mt-24 lg:pb-40 lg:pt-48">
+    <section id="services" className="py-20 lg:pb-40 lg:pt-32">
       <div className="container mx-auto text-center">
         <h2 className="text-3xl lg:text-5xl font-semibold">Our Services</h2>
         <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
@@ -143,16 +143,16 @@ export default () => (
               </p>
               <p className="mt-5 text-lg">
                 We will manage your growth. After setting a timeframe that works for you, our team
-                will provide high-touch servicing for your marketing needs. Whether it’s developing
-                a new strategy, launching campaigns, or creating the assets, we will do what it
-                takes for you to accelerate your growth.
+                will be a part of your team. Whether it’s developing a new strategy, launching
+                campaigns, or creating the assets, we will do what it takes for you to accelerate
+                your growth.
               </p>
             </Card>
           </div>
         </div>
       </div>
     </section>
-    <section id="testimonials" className="py-10 -mt-12 lg:py-40 pb-4">
+    <section id="testimonials" className="py-10 -mt-12 lg:py-32 pb-4">
       <div className="container mx-auto">
         <h2 className="text-3xl lg:text-5xl font-semibold text-center">What Our Clients Say</h2>
         <div className="flex flex-col md:flex-row md:-mx-3 mt-12">
@@ -166,16 +166,16 @@ export default () => (
     </section>
     <SplitSection
       primarySlot={
-        <section id="contact" className="container mx-auto my-30">
-          <h3 className="text-5xl font-semibold">Ready to grow your business?</h3>
+        <section id="contact" className="container mx-auto pt-20 my-30">
+          <h3 className="mt-12 text-4xl font-semibold">Ready to grow your business?</h3>
           <p className="mt-8 text-xl font-light">
             Tell us about your businesses goals and we’ll tell you what we can do for you. Feel free
-            to ask us any questions.
+            to ask us any questsions.
           </p>
           <Contact />
         </section>
       }
-      secondarySlot={<ContactImage />}
+      secondarySlot={<ContactImage className="mt-16 ml-8" />}
     />
   </Layout>
 );
