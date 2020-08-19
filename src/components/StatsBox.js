ac34e481cefd85cd73a5ffaa943fc2607cdb5ec9
Christie Hui <cwsh15@gmail.com>
2020-08-19 13:12:18 -0700

    updated favicon and about section

--- src/components/StatsBox.js
+++ src/components/StatsBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const StatsBox = ({ image, secondaryText }) => (
   <>
-    <img className="rounded-lg flex items-center pb-5 mx-auto" src={image} alt="member" />
+    <img className="rounded-lg flex items-center mb-4 mx-auto" src={image} alt="member" />
     <p className="font-semibold mx-auto px-4 text-lg">{secondaryText}</p>
   </>
 );
