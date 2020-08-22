3fe8694c57c69f57f609f22756bd93fa5df60b08
Christie Hui <cwsh15@gmail.com>
2020-08-22 15:13:03 -0700

    added success page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -58,6 +58,7 @@ class Contact extends Component {
         <form
           name="contact"
           method="POST"
+          action="/success"
           data-netlify="true"
           data-netlify-honeypot="bot-field"
           ref={this.Contact}
--- src/pages/success.js
+++ src/pages/success.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 export default () => (
   <div>
-    <h2>Thanks for contacting us!</h2>
-    <h3>We will make sure to get back to you as soon as possible.</h3>
+    <h3 className="mt-12 text-4xl font-semibold">Thanks for contacting us!</h3>
+    <h3 className="mt-12 text-4xl font-semibold">
+      We will make sure to get back to you as soon as possible.
+    </h3>
   </div>
 );
