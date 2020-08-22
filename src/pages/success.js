895b5792004927c787695d0528a3df6d36e3b168
Christie Hui <cwsh15@gmail.com>
2020-08-22 16:54:19 -0700

    update redirect url

--- src/pages/success.js
+++ src/pages/success.js
@@ -10,12 +10,7 @@ export default () => (
         <h3 className="text-2xl font-semibold leading-tight my-12">
           We&apos;ll make sure to get back to you as soon as possible.
         </h3>
-        <a
-          href="https://hon-media.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-2xl font-semibold pt-12"
-        >
+        <a href="https://hon-media.com" className="text-2xl font-semibold pt-12">
           Back to Hon Media SD
         </a>
       </div>
