ec260bd4b617b919da9b0360afc5fa1a3bb3a94d
Christie Hui <cwsh15@gmail.com>
2020-08-22 16:23:55 -0700

    updated success page and redirect URL

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import SplitSection from '../components/SplitSection';
+import Success from '../svg/Success';
 
 export default () => (
-  <div>
-    <h3 className="mt-12 text-4xl font-semibold">Thanks for contacting us!</h3>
-    <h3 className="mt-12 text-4xl font-semibold">
-      We will make sure to get back to you as soon as possible.
-    </h3>
-  </div>
+  <SplitSection
+    primarySlot={
+      <div className="lg:pr-32 xl:pr-48">
+        <h3 className="text-3xl font-semibold leading-tight">Thanks for contacting us!</h3>
+        <h3 className="text-2xl font-semibold leading-tight my-12">
+          We&apos;ll make sure to get back to you as soon as possible.
+        </h3>
+        <a
+          href="https://hon-media.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-2xl font-semibold pt-12"
+        >
+          Back to Hon Media SD
+        </a>
+      </div>
+    }
+    secondarySlot={<Success />}
+  />
 );
