db28d9f42309b6e5b4df6b457e5e28dea49c175e
Christie Hui <cwsh15@gmail.com>
2020-11-16 13:34:21 -0800

    updated scroll threshold

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-scroll-reveal`,
       options: {
-        threshold: 0.2,
+        threshold: 0.12,
         once: true,
         disable: false
       }
